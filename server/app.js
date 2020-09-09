c404b528c12de02c8459cf80b1a5c517f2b2c459
Aurelius Eggerly <aureliuseggerly@Aureliuss-Air.hsd1.in.comcast.net>
2020-09-09 19:38:17 -0400

    server deploy

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 let express = require('express');
 let app = express();
 let sequelize = require('./db');
+let cors = require('cors')
 
 let user =require('./controllers/usercontroller')
 let review = require('./controllers/reviewcontroller')
 
 sequelize.sync();
 
+app.use(cors())
 app.use(express.json());
 
 
